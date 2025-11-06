ac40ab274765db3f624024b37263d0566a5eae21
Abdul Rauf <abdulraufsiddique10@gmail.com>
2025-11-07 03:59:14 +0500

    Layout: finalize icon meta (v=9) + PNG sizes

--- app/layout.js
+++ app/layout.js
@@ -35,22 +35,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-  {/* existing */}
-  <link rel="icon" href="/favicon.ico?v=9" sizes="any" />
-  <link rel="shortcut icon" href="/favicon.ico?v=9" />
-  <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=9" />
+        {/* Primary icons (served from /public) */}
+        <link rel="icon" href="/favicon.ico?v=9" sizes="any" />
+        <link rel="shortcut icon" href="/favicon.ico?v=9" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=9" />
 
-  {/* extra PNG sizes (optional but nice for some browsers/OSes) */}
-  <link rel="icon" type="image/png" sizes="16x16" href="/icon_16.png" />
-  <link rel="icon" type="image/png" sizes="32x32" href="/icon_32.png" />
-  <link rel="icon" type="image/png" sizes="48x48" href="/icon_48.png" />
-  <link rel="icon" type="image/png" sizes="64x64" href="/icon_64.png" />
-
-  {/* Android/Install surfaces (even without full manifest) */}
-  <link rel="icon" type="image/png" sizes="192x192" href="/icon_192.png" />
-  <link rel="icon" type="image/png" sizes="512x512" href="/icon_512.png" />
-</head>
-
+        {/* Extra PNG sizes for various platforms */}
+        <link rel="icon" type="image/png" sizes="16x16" href="/icon_16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icon_32.png" />
+        <link rel="icon" type="image/png" sizes="48x48" href="/icon_48.png" />
+        <link rel="icon" type="image/png" sizes="64x64" href="/icon_64.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/icon_192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/icon_512.png" />
+      </head>
       <body className="bg-[var(--bg)] text-slate-100 antialiased isolate min-h-screen overflow-x-hidden">
         <main>{children}</main>
       </body>
